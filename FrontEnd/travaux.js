48e80dbe994d456631d6c1a17583fe627e6db4e5
JL8255 <j9660628@gmail.com>
2024-01-28 18:38:01 +0100

    feat : creating category buttons

--- FrontEnd/travaux.js
+++ FrontEnd/travaux.js
@@ -39,3 +39,39 @@ console.log(mapcategories)
 const monSet = new Set();
 mapcategories.forEach((element) => monSet.add(element));
 console.log(monSet)*/
+
+//---------- Création des boutons filtre selon les catégories récupérées -------------------------
+
+//Création du contenu HTML
+//--> insertion de "Tous" dans la liste des catégories
+categories.unshift({ id: 0, name: "Tous" });
+
+//--> Codage de la methode pour les boutons type radio des catégories récupérées
+function generateFiltre(categories) {
+    //Création balise ul
+    const baliseUl = document.createElement("ul");
+    baliseUl.id = "Ul-filtre";
+    //création des balises li avec une boucle for
+    for (let i=0; i < categories.length; i++) {
+        const baliseLi = document.createElement("li");
+            const baliseInput = document.createElement("input");
+                baliseInput.id = categories[i].name;
+                baliseInput.type = "radio";
+                baliseInput.name = "lienfiltre"
+                baliseInput.value = categories[i].name;
+            const baliseLabel = document.createElement("label");
+                baliseLabel.setAttribute("for",categories[i].name);
+                const baliseSpan = document.createElement("span");
+                    baliseSpan.innerText = categories[i].name;
+
+        let sectionFiltre = document.querySelector(".filtre");
+        sectionFiltre.appendChild(baliseUl)
+        baliseUl.appendChild(baliseLi);
+        baliseLi.appendChild(baliseInput);
+        baliseLi.appendChild(baliseLabel);
+        baliseLabel.appendChild(baliseSpan);
+}};
+//--> Appel de la methode pour le 1er affichage
+generateFiltre(categories);
+// sélection du bouton "Tous" par défaut
+const baliseInputTous = document.getElementById("Tous").checked = true;
